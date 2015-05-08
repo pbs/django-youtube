d8962ee5584a4c463e4dcd71ca36d9a0b5a4ae9d
Alex Pirjol <alexandru.pirjol@3pillarglobal.com>
2015-05-08 14:34:28 +0300

    Remove google developer key

--- youtube/settings.py
+++ youtube/settings.py
@@ -3,6 +3,6 @@ from django.conf import settings
 ROOT_URLCONF = getattr(settings, 'ROOT_URLCONF', 'youtube.urls')
 
 DEVELOPER_KEY = getattr(
-    settings, 'GOOGELE_DEVELOPER_KEY', "AIzaSyAodcHoCKVEv7t9782UE5-6PDw-z3Ls1ng")
+    settings, 'GOOGELE_DEVELOPER_KEY', "")
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
